378d236d5384af608d066c81ff29b7dcc4e7f290
Ruiyang Ding <ruiyang.ding@aalto.fi>
2019-01-11 16:24:55 +0200

    meet1

--- store/views.py
+++ store/views.py
@@ -1,14 +1,13 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, Group
 from django.contrib.auth import login
 
-# Create your views here.
-from django.urls import reverse
-
-from store import models
 from store.forms import LoginForm, RegisterForm
+from store.models import Game, Purchase
 
 
 def user_login(request):
@@ -23,17 +22,13 @@ def user_login(request):
                 if user.groups.filter(name='dev').exists():
                     return HttpResponseRedirect('../developer/')
                 elif user.groups.filter(name='player'):
-                    return HttpResponseRedirect('../player/')
+                    return HttpResponseRedirect('../player/games/')
             else:
                 return render(request, 'login.html', {'form': form,'msg':'Username or password is not correct!','can_not_login': True})
         else:
             return render(request, 'login.html', {'form': form, 'msg': 'Username or password is not correct!','can_not_login': True})
     form = LoginForm()
     return render(request, 'login.html', {'form': form, 'can_not_login': False})
-
-def player_list_games(request):
-    return HttpResponse('This is game list of test player')
-
 
 def player_play_game(request, game_id):
     return HttpResponse('This is test player, playing game' + str(game_id))
@@ -55,21 +50,29 @@ def user_register(request):
             password = form.cleaned_data['password']
             email = form.cleaned_data['email']
             group_name = form.cleaned_data['role']
-            print(group_name)
             group = Group.objects.get(name=group_name)
+            try:
+                user = User.objects.create_user(username,email,password)
+                group.user_set.add(user)
+                return HttpResponseRedirect('../login')
+            except IntegrityError:
+                return render(request, 'register.html', {'form': form, 'error': 'Username already exists'})
 
-            user = User.objects.create_user(username,email,password)
-            group.user_set.add(user)
-            return HttpResponseRedirect('../login')
         else:
             return render(request, 'register.html', {'form':form, 'error': 'Please check your username and password if they meet requirements.'})
     form = RegisterForm()
     return render(request,'register.html', {'form':form,'error':''})
 
-
+@login_required()
 def developer_main(request):
+    user = request.user
+    games_sold = Game.objects.filter(developer=user)
+
     return HttpResponse('This is test developer main' + str(request.user))
 
-
+@login_required()
 def player_main(request):
+    user = request.user
+    purchase_history = Purchase.objects.filter(user=user)
+
     return HttpResponse('This is test player main' + str(request.user))
