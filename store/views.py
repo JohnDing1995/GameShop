ec4e3dab4f828ee1ead55af7caa5e9a2991734f1
Pekka Sipilä <pekka.a.sipila@aalto.fi>
2019-01-18 15:23:36 +0200

    Improved navigation, differentiation between create and modify game, store lists all games

--- store/views.py
+++ store/views.py
@@ -35,8 +35,7 @@ def player_play_game(request, game_name):
     return HttpResponse('This is test player, playing game' + str(game_name))
 
 def player_list_games(request):
-    game_list = Game.objects.all()
-    return render(request,'games.html', {'games':game_list})
+    return HttpResponse('This is game list of test player')
 
 
 def developer_modify_game(request, game_name):
@@ -55,7 +54,7 @@ def developer_modify_game(request, game_name):
             return render(request, 'create_game.html', {'form': form, 'msg': 'Form error'})
 
     form = CreateGameForm(initial={'game_url':game.url, 'game_name':game.game_name, 'game_price': game.price})
-    return render(request, 'create_game.html', {'form': form})
+    return render(request, 'create_game.html', {'form': form, "action":"Modify game"})
 
 
 def user_register(request):
@@ -114,15 +113,15 @@ def developer_create_game(request):
             url = form.cleaned_data['game_url']
             if len(Game.objects.filter(game_name=game_name)) > 0:
                 print('Game already exists')
-                return render(request, "create_game.html", {'form': form, 'msg':'Game already exists'})
+                return render(request, "create_game.html", {'form': form, 'msg':'Game already exists', "action":"Create game"})
             else:
                 g = Game(game_name=game_name, price=price, developer=user, copies_sold=0, url=url)
                 g.save()
-                return render(request, "create_game.html", {'form': form, 'msg': 'Game created'})
+                return render(request, "create_game.html", {'form': form, 'msg': 'Game created', "action":"Create game"})
         else:
-            return render(request, "create_game.html", {'form': form, 'msg': 'Illegal input'})
+            return render(request, "create_game.html", {'form': form, 'msg': 'Illegal input', "action":"Create game"})
     form = CreateGameForm()
-    return render(request, "create_game.html", {'form': form})
+    return render(request, "create_game.html", {'form': form, "action":"Create game"})
 
 @login_required(login_url='/login')
 def developer_game_buyer(request, game_name):
@@ -136,4 +135,4 @@ def store(request):
         print("Not player")
         return redirect('developer_main')
     all_games = Game.objects.all()
-    return render(request, "store.html", {'games', all_games})
+    return render(request, "store.html", {'games': all_games})
