a0b5daa9e5b1954283595c3d76afcd1eceec33f2
Ling Sun <zero9654@live.com>
2019-01-25 21:56:15 +0200

    update

--- store/player_urls.py
+++ store/player_urls.py
@@ -4,9 +4,6 @@ urlpatterns = [
     path('', views.player_main, name= 'player_main'),
     path('store', views.store, name='store'),
     path('games/<game_name>', views.player_play_game),
-    path('games/<game_name>/buy', views.player_buy_game),
-    path('games/<game_name>/save', views.player_save_game),
-    path('games/<game_name>/load', views.player_load_game),
-    path('games/<game_name>/submit_score', views.player_submit_score),
+    path('games/<game_name>/buy', views.player_buy_game)
 
 ]
