01dced55eb2bac198182e826f30f4bd088db27ce
Ling Sun <zero9654@live.com>
2019-01-26 00:16:44 +0200

    update

--- store/developer_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from store import views
-urlpatterns = [
-    path('', views.developer_main,  name = 'developer_main'),
-    path('create_game', views.developer_create_game),
-    path('games', views.developer_sales),
-    path('games/<game_name>/modify', views.developer_modify_game)
-]
--- store/player_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from store import views
-urlpatterns = [
-    path('', views.player_main, name= 'player_main'),
-    path('store', views.store, name='store'),
-    path('games/<game_name>', views.player_play_game),
-    path('games/<game_name>/buy', views.player_buy_game)
-
-]
